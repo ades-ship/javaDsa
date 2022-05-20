269e464f2334feb7ce4083066e9a01ba22bde92f
Adesh Sengar <adeshsengar156@gmail.com>
2022-05-20 22:56:39 +0530

    -

--- FirstClass.java
+++ FirstClass.java
@@ -1,5 +1,19 @@
-public class FirstClass {
+ class FirstClass {
     public static void main(String []k){
         System.out.println("hello world");
     }
 }
+
+class Main{
+    public static void main(String []args){
+        System.out.println("hello world let do the coding ");
+        // for same line
+        System.out.print("adesh");
+        System.out.print("sengar");
+        System.out.print("\nsengar");
+
+
+    }
+}
+
+
